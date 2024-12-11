b142c43166731deb3135c32f32d28bc2eeb00459
rangapv <rangapv@yahoo.com>
2024-12-11 13:20:11 +0000

    Updated cron callable file

--- secondrunthis.sh
+++ secondrunthis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 OUTPUT=`ps -ef |grep 'node app.js'|grep -v grep`
+cmd=`cp /home/ubuntu/node2/app.js.https /home/ubuntu/node2/app.js`
 cmd1="cd /home/ubuntu/node2; nohup sudo node app.js 2>&1 &"
 if [ -z "$OUTPUT" ]
 then
